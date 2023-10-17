ffb1a149551db496ca32cf3f5189520de532f8d5
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-10-17 18:19:19 +0530

    🧪 Update tests' name and format scripts

--- tests/test_from_uncurl.py
+++ tests/test_from_uncurl.py
@@ -1,93 +1,63 @@
 import unittest
 
 from curler import curl_command_parser
-from curler.constants import Method
 from curler.typing import ParsedCurl
-
-http_method = Method()
 
 
 class TestCurlCommands(unittest.TestCase):
-    def test_curl_command_1(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            """--data '[{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"},"now":1396219192277,"ab":{"welcome_email":{"v":"2","g":2}}}]' """
-            "-H 'Accept-Encoding: gzip,deflate,sdch' "
-            "-H 'Cookie: foo=bar; baz=baz2'"
-        )
+    def test_curl_parser_ability(self):
+        curl_command = """curl 'https://pypi.org/p/curler' \
+            --data '[{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"},"now":1396219192277,"ab":{"welcome_email":{"v":"2","g":2}}}]' \
+            -H 'Accept-Encoding: gzip,deflate,sdch' \
+            -H 'Cookie: foo=bar; baz=baz2'"""
         parsed = curl_command_parser(curl_command)
         expected = ParsedCurl(
             method="GET",
-            url="https://pypi.python.org/pypi/uncurl",
+            url="https://pypi.org/p/curler",
             data='[{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"},"now":1396219192277,"ab":{"welcome_email":{"v":"2","g":2}}}]',
-            headers={
-                "Accept-Encoding": "gzip,deflate,sdch",
-            },
+            headers={"Accept-Encoding": "gzip,deflate,sdch"},
             cookies={"foo": "bar", "baz": "baz2"},
         )
         self.assertEqual(parsed, expected)
 
-    def test_curl_command_2(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            """--data '{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"}}' """
-            "-H 'Accept-Encoding: gzip,deflate,sdch' "
-            "-H 'Cookie: foo=bar; baz=baz2'"
-        )
+    def test_data_parsing(self):
+        curl_command = """curl 'https://pypi.org/p/curler' \
+            --data '{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"}}' \
+            -H 'Accept-Encoding: gzip,deflate,sdch' \
+            -H 'Cookie: foo=bar; baz=baz2'"""
         parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(
-            method="GET",
-            url="https://pypi.python.org/pypi/uncurl",
-            data='{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"}}',
-            headers={
-                "Accept-Encoding": "gzip,deflate,sdch",
-            },
-            cookies={"foo": "bar", "baz": "baz2"},
+        self.assertEqual(
+            parsed.data,
+            r'{"evt":"newsletter.show","properties":{"newsletter_type":"userprofile"}}',
         )
-        self.assertEqual(parsed, expected)
 
-    def test_curl_command_3(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            "--data 'this is just some data'"
-        )
+    def test_data_binary_parsing(self):
+        curl_command = """curl 'https://pypi.org/p/curler' \
+            -X 'POST' \
+            --data-binary 'this is just some data'"""
         parsed = curl_command_parser(curl_command)
-        self.assertEqual(parsed.method, http_method.GET)
-
-    def test_curl_command_4(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            "-X 'POST' "
-            "--data-binary 'this is just some data'"
-        )
-        parsed = curl_command_parser(curl_command)
-
-        self.assertEqual(parsed.method, http_method.POST)
         self.assertEqual(parsed.data_binary, "this is just some data")
 
-    def test_curl_command_5(self):
-        curl_command = (
-            "curl "
-            "'http://pandahomeios.ifjing.com/action.ashx/otheraction/9028' "
-            "-H 'PID: 20000079' "
-            "-H 'MT: 4' "
-            "-H 'DivideVersion: 1.0' "
-            "-H 'SupPhone: Redmi Note 3' "
-            "-H 'SupFirm: 5.0.2' "
-            "-H 'IMEI: wx_app' "
-            "-H 'IMSI: wx_app' "
-            "-H 'SessionId: ' "
-            "-H 'CUID: wx_app' "
-            "-H 'ProtocolVersion: 1.0' "
-            "-H 'Sign: 7876480679c3cfe9ec0f82da290f0e0e' "
-            "-H 'Accept: /' "
-            "-H 'BodyEncryptType: 0' "
-            """-H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; OPPO R9s Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36 hap/1.0/oppo com.nearme.instant.platform/2.1.0beta1 com.felink.quickapp.reader/1.0.3 ({"packageName":"com.oppo.market","type":"other","extra":{}})' """
-            "-H 'Content-Type: text/plain; charset=utf-8' "
-            "-H 'Host: pandahomeios.ifjing.com' "
-            """--data-binary '{"CateID":"508","PageIndex":1,"PageSize":30}' """
-            "--compressed"
-        )
+    def test_big_command_parsing(self):
+        curl_command = """curl 'http://pandahomeios.ifjing.com/action.ashx/otheraction/9028' \
+            -H 'PID: 20000079' \
+            -H 'MT: 4' \
+            -H 'DivideVersion: 1.0' \
+            -H 'SupPhone: Redmi Note 3' \
+            -H 'SupFirm: 5.0.2' \
+            -H 'IMEI: wx_app' \
+            -H 'IMSI: wx_app' \
+            -H 'SessionId: ' \
+            -H 'CUID: wx_app' \
+            -H 'ProtocolVersion: 1.0' \
+            -H 'Sign: 7876480679c3cfe9ec0f82da290f0e0e' \
+            -H 'Accept: /' \
+            -H 'BodyEncryptType: 0' \
+            -H 'User-Agent: Mozilla/5.0 (Linux; Android 6.0.1; OPPO R9s Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36 hap/1.0/oppo com.nearme.instant.platform/2.1.0beta1 com.felink.quickapp.reader/1.0.3 ({"packageName":"com.oppo.market","type":"other","extra":{}})' \
+            -H 'Content-Type: text/plain; charset=utf-8' \
+            -H 'Host: pandahomeios.ifjing.com' \
+            --data-binary '{"CateID":"508","PageIndex":1,"PageSize":30}' \
+            --compressed"""
         parsed = curl_command_parser(curl_command)
         expected = ParsedCurl(
             method="POST",
@@ -115,20 +85,16 @@ class TestCurlCommands(unittest.TestCase):
         )
         self.assertEqual(parsed, expected)
 
-    def test_curl_command_7(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            "-H 'Accept-Encoding: gzip,deflate' "
-            "--insecure"
-        )
+    def test_insecure_param_parsing(self):
+        curl_command = """curl 'https://pypi.org/p/curler' \
+            -H 'Accept-Encoding: gzip,deflate' \
+            --insecure"""
         parsed = curl_command_parser(curl_command)
         self.assertEqual(parsed.insecure, True)
 
-    def test_curl_command_8(self):
-        curl_command = (
-            "curl 'https://pypi.python.org/pypi/uncurl' "
-            r"-H $'cookie: sid=00Dt00000004XYz\u0021ARg'"
-        )
+    def test_cookies_with_encoded_character(self):
+        curl_command = r"""curl 'https://pypi.org/p/curler'
+            -H $'cookie: sid=00Dt00000004XYz\u0021ARg'"""
         parsed = curl_command_parser(curl_command)
         self.assertEqual(parsed.cookies, {"sid": "00Dt00000004XYz!ARg"})
 
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,61 +1,49 @@
 import unittest
 
 from curler.parser import curl_command_parser
-from curler.typing import ParsedCurl
 
 
 class TestCurlParser(unittest.TestCase):
     def test_get_request(self):
         curl_command = "curl http://example.com"
         parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(method="GET", url="http://example.com")
-        self.assertEqual(parsed, expected)
+        self.assertEqual(parsed.method, "GET")
 
     def test_post_request(self):
         curl_command = 'curl -X POST -d "data" http://example.com'
         parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(method="POST", url="http://example.com", data="data")
-        self.assertEqual(parsed, expected)
-
-    def test_custom_headers(self):
-        curl_command = 'curl -H "Content-Type: application/json" -H "Authorization: Bearer token" http://example.com'
-        parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(
-            method="GET",
-            url="http://example.com",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": "Bearer token",
-            },
-        )
-        self.assertEqual(parsed, expected)
+        self.assertEqual(parsed.method, "POST")
 
     def test_cookies_and_headers(self):
-        curl_command = 'curl -H "Cookie: cookie1=value1; cookie2=value2" -H "Authorization: Bearer token" http://example.com'
+        curl_command = """curl \
+            -H "Cookie: cookie1=value1; cookie2=value2" \
+            -H "Authorization: Bearer token" \
+            -H "Content-Type: application/json" \
+            http://example.com"""
         parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(
-            method="GET",
-            url="http://example.com",
-            headers={"Authorization": "Bearer token"},
-            cookies={"cookie1": "value1", "cookie2": "value2"},
+        self.assertEqual(parsed.cookies, {"cookie1": "value1", "cookie2": "value2"})
+        self.assertEqual(
+            parsed.headers,
+            {
+                "Authorization": "Bearer token",
+                "Content-Type": "application/json",
+            },
         )
-        self.assertEqual(parsed, expected)
 
     def test_proxy_and_user_auth(self):
-        curl_command = (
-            "curl -x proxy.example.com:8080 -U user:password http://example.com"
-        )
+        curl_command = """curl \
+            -x proxy.example.com:8080 \
+            -U user:password \
+            http://example.com"""
         parsed = curl_command_parser(curl_command)
-        expected = ParsedCurl(
-            method="GET",
-            url="http://example.com",
-            user=("user", "password"),
-            proxy={
+        self.assertEqual(parsed.user, ("user", "password"))
+        self.assertEqual(
+            parsed.proxy,
+            {
                 "http": "http://user:password@proxy.example.com:8080/",
                 "https": "https://user:password@proxy.example.com:8080/",
             },
         )
-        self.assertEqual(parsed, expected)
 
     def test_not_implemented_method(self):
         curl_command = "curl -X PUT http://example.com"
